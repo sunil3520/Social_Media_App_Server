5e32bc018ade30c4ea29663cd109e3e9d68c67cd
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2024-02-19 20:44:52 +0530

    update:updated register-route

--- Middlewares/LoginMiddleware.js
+++ Middlewares/LoginMiddleware.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken");
 const LoginMiddleware = (req, res, next) => {
   const token = req.headers.auth;
-
+ console.log(token,"token")
   if (token) {
     const decode = jwt.verify(token, "loginornot");
-  
+   console.log(decode,'decode')
     if (decode) {
       req.body.userId=decode.userId;
+      console.log(decode.userId,"userid")
       next();
     }
   } else {
